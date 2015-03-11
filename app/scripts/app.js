c5c5f4165eb3fe321eb21d1bf50d10f58b124cb2
Giovanni Charles <giovanni.charles@gmail.com>
2015-03-11 17:05:24 +0000

    angular generated

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name electionhackApp
+ * @description
+ * # electionhackApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('electionhackApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
