f76cf0e769ac413b314bdb85c64e25c13ff43997
Giovanni Charles <giovanni.charles@gmail.com>
2015-03-15 17:01:34 +0000

    generated

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name electionhackApp
+ * @description
+ * # electionhackApp
+ *
+ * Main module of the application.
+ */
+angular.module('electionhackApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.utils',
+    'simpleLogin'
+  ]);
