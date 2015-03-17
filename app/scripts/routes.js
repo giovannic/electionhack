e35951b5838f919c1e83e3f9cc5f741eeeb35ba9
ben-chin <ben.chin101@gmail.com>
2015-03-17 23:56:45 +0000

    Fixed authrequired routing bug

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -108,7 +108,7 @@ angular.module('electionhackApp')
       // some of our routes may reject resolve promises with the special {authRequired: true} error
       // this redirects to the login page whenever that is encountered
       $rootScope.$on('$routeChangeError', function(e, next, prev, err) {
-        if( angular.isObject(err) && err.authRequired ) {
+        if( err === "AUTH_REQUIRED" ) {
           $location.path(loginRedirectPath);
         }
       });
