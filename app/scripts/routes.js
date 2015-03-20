a4e791123953e1f3a4d2a71066bd253161bc5d3f
ben-chin <ben.chin101@gmail.com>
2015-03-20 02:18:29 +0000

    Make jshint happy

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -113,7 +113,7 @@ angular.module('electionhackApp')
       // some of our routes may reject resolve promises with the special {authRequired: true} error
       // this redirects to the login page whenever that is encountered
       $rootScope.$on('$routeChangeError', function(e, next, prev, err) {
-        if( err === "AUTH_REQUIRED" ) {
+        if( err === 'AUTH_REQUIRED' ) {
           $location.path(loginRedirectPath);
         }
       });
